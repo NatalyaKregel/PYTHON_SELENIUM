f75545246d624146d0571398e75b28b5a5fd4ecb
NatalyaKregel <111009881+NatalyaKregel@users.noreply.github.com>
2023-10-07 01:39:57 +0300

    add tasks final sertification

--- FINAL_CTRIFICATION/test_api.py
+++ FINAL_CTRIFICATION/test_api.py
@@ -1,1 +1,18 @@
+import logging
+import pytest
+from apipage import RestAPI
+
+
+def test_api_username(expected_username):
+    logging.info('Authorization on the site with using an authorization token')
+    api_client = RestAPI()
+
+    token = api_client.login()
+    received_username = api_client.get_username(token)
+    assert received_username == expected_username, logging.exception(f'{expected_username} not found in list')
+
+
+if __name__ == '__main__':
+    pytest.main(['-vv'])
+
 
