30dedce07f4f4137a96f5fbcac091b3688213d28
NatalyaKregel <111009881+NatalyaKregel@users.noreply.github.com>
2023-10-07 01:41:49 +0300

    add tasks final sertification

--- FINAL_CTRIFICATION/test_nikto.py
+++ FINAL_CTRIFICATION/test_nikto.py
@@ -1,1 +1,9 @@
+import logging
+from BaseApp import WebServerEvaluation
 
+
+def test_nikto_vulnerability(get_nikto_command, expected_result):
+    logging.info('Check a site for vulnerabilities using a command utility lines')
+    web_server_evaluator = WebServerEvaluation()
+    received_text = web_server_evaluator.executing_nikto_command(get_nikto_command)
+    assert expected_result in received_text, logging.exception(f'Error: {expected_result} not found')
